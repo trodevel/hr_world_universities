c7f5233c10477f52a0fc855db475a252c774b754
trodevel <trodevel@gmail.com>
2023-09-20 20:36:58 +0200

    added output

--- tools/parse_universities.py
+++ tools/parse_universities.py
@@ -1,16 +1,19 @@
 import csv
 
-def read_file( filename: str ):
+def read_file( filename: str, outp_filename: str ):
 
-    num_lines = 0
+    i = 0
+
+    f = open( outp_filename, "w" )
+
 
     with open( filename ) as csvfile:
         reader = csv.reader( csvfile, delimiter=',' )
+        i += 1
         for row in reader:
             ( country, name, url ) = row[0:3]
-            #print( "DEBUG: {}, {}, {}".format( username, timestamp, follow_status ) )
+            f.write( f"{i};{country};{name};{url}\n" )
 
-    print( f"INFO: read {num_lines} records from {filename}" )
+    print( f"INFO: read {i} records from {filename}, wrote result to {outp_filename}" )
 
-
-
+read_file( "../externals/world-universities-csv/world-universities.csv", "../resources/universities.csv" )
